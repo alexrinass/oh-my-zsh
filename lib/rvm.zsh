6f8dc3b612c8f6695da1a72c77a97b29c9cf6939
Alexander Rinass <alex@rinass.net>
2011-04-14 20:00:34 +0200

    Improved RVM prompt info to be more customizable.

--- lib/rvm.zsh
+++ lib/rvm.zsh
@@ -1,7 +1,10 @@
 # get the name of the branch we are on
 function rvm_prompt_info() {
   ruby_version=$(~/.rvm/bin/rvm-prompt 2> /dev/null) || return
-  echo "($ruby_version)"
+  
+  if [ -z "$ruby_version" ]; then
+    echo ""
+  else
+    echo "$RVM_THEME_GIT_PROMPT_PREFIX$ruby_version$RVM_THEME_GIT_PROMPT_SUFFIX"
+  fi
 }
-
-
