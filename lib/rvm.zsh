48275d154d7906f0624b04ce70d17660227b51ab
Alexander Rinass <alex@rinass.net>
2011-04-20 15:56:23 +0200

    Fixed variable typo in rvm lib and added variable defaults.

--- lib/rvm.zsh
+++ lib/rvm.zsh
@@ -1,10 +1,22 @@
+if [ -z $ZSH_THEME_RVM_PROMPT_NONE ]; then
+  export ZSH_THEME_RVM_PROMPT_NONE=""
+fi
+
+if [ -z $ZSH_THEME_RVM_PROMPT_PREFIX ]; then
+  export ZSH_THEME_RVM_PROMPT_PREFIX="rvm("
+fi
+
+if [ -z $ZSH_THEME_RVM_PROMPT_SUFFIX ]; then
+  export ZSH_THEME_RVM_PROMPT_SUFFIX=")"
+fi
+
 # get the name of the branch we are on
 function rvm_prompt_info() {
   ruby_version=$(~/.rvm/bin/rvm-prompt 2> /dev/null) || return
   
   if [ -z "$ruby_version" ]; then
-    echo ""
+    echo "$ZSH_THEME_RVM_PROMPT_NONE"
   else
-    echo "$RVM_THEME_GIT_PROMPT_PREFIX$ruby_version$RVM_THEME_GIT_PROMPT_SUFFIX"
+    echo "$ZSH_THEME_RVM_PROMPT_PREFIX$ruby_version$ZSH_THEME_RVM_PROMPT_SUFFIX"
   fi
 }
