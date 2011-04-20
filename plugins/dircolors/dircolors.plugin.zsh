79ff3204a40319871602afb9af754e12ce4c1c9c
Alexander Rinass <alex@rinass.net>
2011-04-20 16:07:53 +0200

    Minor improvements to the dircolors plugin.

--- plugins/dircolors/dircolors.plugin.zsh
+++ plugins/dircolors/dircolors.plugin.zsh
@@ -2,5 +2,10 @@ if [ -x /opt/local/bin/gdircolors ]; then
   alias dircolors='/opt/local/bin/gdircolors'
 fi
 
+if [ -x /opt/local/bin/gls ]; then
+  alias ls='/opt/local/bin/gls --color=auto'
+fi
+
 # Set terminal display colors
-eval `dircolors $ZSH/plugins/dircolors/themes/alex.dircolors-theme`
+# TODO: check $ZSH_DIRCOLORS_THEME
+eval `dircolors $ZSH/plugins/dircolors/themes/$ZSH_DIRCOLORS_THEME.dircolors-theme`
