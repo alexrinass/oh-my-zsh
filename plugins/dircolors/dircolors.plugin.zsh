13dfb8aacb93382f7ed145e571dcc8a6280cc1d6
Alexander Rinass <alex@fournova.com>
2012-08-29 10:02:06 +0200

    Fix issue with dircolors plugin.

--- plugins/dircolors/dircolors.plugin.zsh
+++ plugins/dircolors/dircolors.plugin.zsh
@@ -1,13 +1,13 @@
-DIRCOLORS=$(which gdircolors)
-
-if [ -z "$DIRCOLORS" ]; then
-  DIRCOLORS=$(which dircolors)
+if which gdircolors &> /dev/null; then
+	DIRCOLORS=$(which gdircolors)	
+elif which dircolors &> /dev/null; then
+	DIRCOLORS=$(which dircolors)
 fi
 
-LS=$(which gls)
-
-if [ -z "$LS" ]; then
-  LS=$(which ls)
+if which gls &> /dev/null; then
+	LS=$(which gls)	
+elif which ls &> /dev/null; then
+	LS=$(which ls)
 fi
 
 if [ -x "$LS" ]; then
