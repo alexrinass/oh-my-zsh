2d45bb726a39fc36dfd7321d64b14d7f2b84f8cb
Alexander Rinass <alex@fournova.com>
2012-09-04 11:22:12 +0200

    Remove macports aliases.

--- share/custom/macports_aliases.zsh
+++
@@ -1,3 +0,0 @@
-alias _php.ini='mate /opt/local/etc/php5/php.ini'
-alias _httpd.conf='mate /opt/local/apache2/conf/httpd.conf'
-alias _vhosts.conf='mate /opt/local/apache2/conf/extra/httpd-vhosts.conf'
