b6e72fb39448d43fbc4a39508f640bac440fde13
Alexander Rinass <alex@fournova.com>
2012-09-19 14:38:38 +0200

    Remove theme switcher for SSH connections.

--- share/custom/macosx_aliases.zsh
+++ share/custom/macosx_aliases.zsh
@@ -1,17 +1,2 @@
 alias _hosts='mate /etc/hosts'
 alias flushdns='sudo dscacheutil -flushcache'
-
-function chtheme() {
-  NAME=$1; if [ -z "$NAME" ]; then NAME="Default"; fi
-  osascript -e "tell application \"Terminal\" to set current settings of front window to settings set \"$NAME\""
-}
-
-function ssh() {
-	if [ ! -z "$1" ]; then
-		chtheme "Solarized Light"	
-		/usr/bin/ssh "$@"
-		chtheme "Solarized Dark"
-	else
-		/usr/bin/ssh "$@"
-	fi	
-}
